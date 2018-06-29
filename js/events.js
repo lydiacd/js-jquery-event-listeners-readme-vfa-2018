7505e2eb141bbadc4d87a6bb9bf89667aec7599a
Lydia Dick <lydiadick@gmail.com>
2018-06-29 20:55:19 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,9 +2,14 @@
 
 // 1. Define a function getIt that does not accept a parameter. The function should bind a click event to the p tag. When the paragraph is clicked, the function should alert "Hey!".
 
-$('p').on('keypress', f)
+$('p').on('clicked', function getIt(){
+  return "Hey!"
+});
 
 // 2. Define a function frameIt that does not accept a parameter. The function should bind the load event that adds the class tasty to the image to add a red frame to the image.
+$('img').on('load', function frameIt() {
+  retrun img+.tasty
+}
 
 // 3. Define a function pressIt that does not accept a parameter. The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key.
 
