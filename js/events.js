c3acc44a97b07ea184f3c3bbbb5769d7f6093944
Lydia Dick <lydiadick@gmail.com>
2018-06-29 20:42:00 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,7 +4,9 @@
 
 // 2. Define a function frameIt that does not accept a parameter. The function should bind the load event that adds the class tasty to the image to add a red frame to the image.
 
-// 3. 
+// 3. Define a function pressIt that does not accept a parameter. The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key.
+
+//
 $(document).ready(function(){
 
 // call functions here
