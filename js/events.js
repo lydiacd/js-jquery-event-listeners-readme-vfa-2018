d709b2990f647690d6004ef3f1a22940441dfc01
Lydia Dick <lydiadick@gmail.com>
2018-06-29 20:41:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,7 +2,9 @@
 
 // 1. Define a function getIt that does not accept a parameter. The function should bind a click event to the p tag. When the paragraph is clicked, the function should alert "Hey!".
 
-// 2. 
+// 2. Define a function frameIt that does not accept a parameter. The function should bind the load event that adds the class tasty to the image to add a red frame to the image.
+
+// 3. 
 $(document).ready(function(){
 
 // call functions here
